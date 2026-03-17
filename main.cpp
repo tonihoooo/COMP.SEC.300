696a9bee9c0bf3012a0da7ffb3c238060d972883
Toni Heikkila <toni.heikkila@tuni.fi>
2026-03-17 08:46:15 +0200

    Added master password which is used with XOR for simple encryption/decryption

--- main.cpp
+++ main.cpp
@@ -10,14 +10,24 @@ struct Entry {
     std::string password;
 };
 
-// Serialize vault to a binary file
-void save_vault(const std::string& filename, const std::vector<Entry>& vault) {
+// XOR encryption/decryption
+std::string xor_encrypt_decrypt(const std::string& data, const std::string& key) {
+    std::string result = data;
+    for (size_t i = 0; i < data.size(); ++i) {
+        result[i] ^= key[i % key.size()]; 
+    }
+    return result;
+}
+
+// Save vault to file (with XOR)
+void save_vault(const std::string& filename, const std::vector<Entry>& vault, const std::string& master_password) {
     std::ofstream ofs(filename, std::ios::binary);
     for (const auto& e : vault) {
-        auto write_string = [&ofs](const std::string& s) {
-            uint32_t len = s.size();
+        auto write_string = [&ofs, &master_password](const std::string& s) {
+            std::string encrypted = xor_encrypt_decrypt(s, master_password);
+            uint32_t len = encrypted.size();
             ofs.write(reinterpret_cast<const char*>(&len), sizeof(len));
-            ofs.write(s.data(), len);
+            ofs.write(encrypted.data(), len);
         };
         write_string(e.name);
         write_string(e.username);
@@ -25,19 +35,19 @@ void save_vault(const std::string& filename, const std::vector<Entry>& vault) {
     }
 }
 
-// Load vault from a binary file
-std::vector<Entry> load_vault(const std::string& filename) {
+// Load vault from file (with XOR)
+std::vector<Entry> load_vault(const std::string& filename, const std::string& master_password) {
     std::vector<Entry> vault;
     std::ifstream ifs(filename, std::ios::binary);
     if (!ifs) return vault;
 
     while (ifs.peek() != EOF) {
-        auto read_string = [&ifs]() -> std::string {
+        auto read_string = [&ifs, &master_password]() -> std::string {
             uint32_t len;
             ifs.read(reinterpret_cast<char*>(&len), sizeof(len));
             std::string s(len, '\0');
             ifs.read(&s[0], len);
-            return s;
+            return xor_encrypt_decrypt(s, master_password);
         };
         Entry e;
         e.name = read_string();
@@ -56,7 +66,7 @@ int main() {
     std::cin >> master_password;
 
     // Load vault
-    std::vector<Entry> vault = load_vault(filename);
+    std::vector<Entry> vault = load_vault(filename, master_password);
     std::cout << "\nVault loaded (" << vault.size() << " entries).\n";
 
     std::string command;
@@ -71,7 +81,7 @@ int main() {
             std::cout << "Username: "; std::cin >> e.username;
             std::cout << "Password: "; std::cin >> e.password;
             vault.push_back(e);
-            save_vault(filename, vault);
+            save_vault(filename, vault, master_password);
             std::cout << "Entry added and saved.\n";
 
         } else if (command == "list") {
